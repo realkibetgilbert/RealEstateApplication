200a0b75f4c1de58afe726af663e5c3c0b5bc317
Gilbert Kibet <kibetgilly354@gmail.com>
2025-07-08 18:32:21 +0300

    updated routes in the gateway

--- 3.TenantService/TenantService/Tenant.API.Application/Common/ServiceResponse.cs
+++ 3.TenantService/TenantService/Tenant.API.Application/Common/ServiceResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tenant.API.Application.Common
+﻿namespace Tenant.API.Application.Common
 {
     public class ServiceResponse<T>
     {
